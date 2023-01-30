ddf428102ebca363e178ee374aa07aa88b8d7932
niraj <nirajkkushwaha1@gmail.com>
2023-01-30 13:26:20 +0100

    added year range selector in data loader

--- arcolanche/data.py
+++ arcolanche/data.py
@@ -25,25 +25,34 @@ class ACLED2020():
         df.sort_values('EVENT_DATE', inplace=True)
 
     @classmethod
-    def battles_df(cls, pre_covid=True, to_lower=False):
+    def battles_df(cls, pre_covid=True, to_lower=False, year_range=False):
         """
         Parameters
         ----------
         pre_covid : bool, True
         to_lower : bool, False
             If True, rename all cols to lower case.
+        year_range : tuple , False
+            If a tuple is passed, the first element of the tuple is taken
+            to be the lower cutoff of year and second element is upper
+            cutoff.
 
         Returns
         -------
         pd.DataFrame
         """
 
-        if pre_covid:
-            ix = ((cls.df['EVENT_DATE']>=pd.to_datetime('1997/01/01')) &
-                  (cls.df['EVENT_DATE']<=pd.to_datetime('2019/12/31')))
+        if(year_range):
+            ix = ((cls.df['EVENT_DATE']>=pd.to_datetime(f'{year_range[0]}/01/01')) &
+                  (cls.df['EVENT_DATE']<=pd.to_datetime(f'{year_range[1]}/12/31')))
             df = cls.df.loc[ix]
         else:
-            df = cls.df
+            if pre_covid:
+                ix = ((cls.df['EVENT_DATE']>=pd.to_datetime('1997/01/01')) &
+                    (cls.df['EVENT_DATE']<=pd.to_datetime('2019/12/31')))
+                df = cls.df.loc[ix]
+            else:
+                df = cls.df
         
         if to_lower:
             df.columns = [i.lower() for i in df.columns]
@@ -51,23 +60,34 @@ class ACLED2020():
         return df.loc[df['EVENT_TYPE']=='Battles']
 
     @classmethod
-    def vac_df(cls, pre_covid=True, to_lower=False):
+    def vac_df(cls, pre_covid=True, to_lower=False, year_range=False):
         """
         Parameters
         ----------
         pre_covid : bool, True
+        to_lower : bool, False
+            If True, rename all cols to lower case.
+        year_range : tuple , False
+            If a tuple is passed, the first element of the tuple is taken
+            to be the lower cutoff of year and second element is upper
+            cutoff.
 
         Returns
         -------
         pd.DataFrame
         """
 
-        if pre_covid:
-            ix = ((cls.df.EVENT_DATE>=pd.to_datetime('1997/01/01')) &
-                  (cls.df.EVENT_DATE<=pd.to_datetime('2019/12/31')))
+        if(year_range):
+            ix = ((cls.df['EVENT_DATE']>=pd.to_datetime(f'{year_range[0]}/01/01')) &
+                  (cls.df['EVENT_DATE']<=pd.to_datetime(f'{year_range[1]}/12/31')))
             df = cls.df.loc[ix]
         else:
-            df = cls.df
+            if pre_covid:
+                ix = ((cls.df['EVENT_DATE']>=pd.to_datetime('1997/01/01')) &
+                    (cls.df['EVENT_DATE']<=pd.to_datetime('2019/12/31')))
+                df = cls.df.loc[ix]
+            else:
+                df = cls.df
 
         if to_lower:
             df.columns = [i.lower() for i in df.columns]
@@ -75,23 +95,34 @@ class ACLED2020():
         return df.loc[df['EVENT_TYPE']=='Violence against civilians']
 
     @classmethod
-    def riots_and_protests_df(cls, pre_covid=True, to_lower=False):
+    def riots_and_protests_df(cls, pre_covid=True, to_lower=False, year_range=False):
         """
         Parameters
         ----------
         pre_covid : bool, True
+        to_lower : bool, False
+            If True, rename all cols to lower case.
+        year_range : tuple , False
+            If a tuple is passed, the first element of the tuple is taken
+            to be the lower cutoff of year and second element is upper
+            cutoff.
 
         Returns
         -------
         pd.DataFrame
         """
 
-        if pre_covid:
-            ix = ((cls.df.EVENT_DATE>=pd.to_datetime('1997/01/01')) &
-                  (cls.df.EVENT_DATE<=pd.to_datetime('2019/12/31')))
+        if(year_range):
+            ix = ((cls.df['EVENT_DATE']>=pd.to_datetime(f'{year_range[0]}/01/01')) &
+                  (cls.df['EVENT_DATE']<=pd.to_datetime(f'{year_range[1]}/12/31')))
             df = cls.df.loc[ix]
         else:
-            df = cls.df
+            if pre_covid:
+                ix = ((cls.df['EVENT_DATE']>=pd.to_datetime('1997/01/01')) &
+                    (cls.df['EVENT_DATE']<=pd.to_datetime('2019/12/31')))
+                df = cls.df.loc[ix]
+            else:
+                df = cls.df
 
         if to_lower:
             df.columns = [i.lower() for i in df.columns]
